1bfcbd4ee99141ede791784b0268e7c174bf8b0b
Angelo Luciani <mistereal@gmail.com>
2015-08-14 12:15:27 +0200

    added totalb
    added totalb

---
+++ totalb/src/com/angeloluciani/totalb/Person.java
@@ -0,0 +1,14 @@
+package com.angeloluciani.totalb;
+
+public class Person {
+	//fields
+	private String name; //name of the person
+	private int maximumBooks; // most books the person can checkout
+	
+	//constructor
+	public Person()
+	{
+		name = "Unknow name";
+		maximumBooks = 3;
+	}
+}
